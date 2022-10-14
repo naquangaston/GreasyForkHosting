dba705bd923453b6b0569f3842cf55479c807268
Gaston <35858201+naquangaston@users.noreply.github.com>
2022-10-14 07:07:13 -0400

    Create uploader.js

---
+++ uploader.js
@@ -0,0 +1,41 @@
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  let user = getCookie("username");
+  if (user != "") {
+    alert("Welcome again " + user);
+  } else {
+    user = prompt("Please enter your name:", "");
+    if (user != "" && user != null) {
+      setCookie("username", user, 365);
+    }
+  }
+}
+var json=await fetch(`https://greasyfork.org/en/scripts/440929.json`).then(J=>J.json())
+if(json.version==this.version){
+    console.log('No updates found');
+}else if(json.version>this.version){
+    console.log('Version downgrade')
+    confirm('The current script version was downgraded');
+    setCookie('asked',true)
+}else{console.log('new update avaliable')}
